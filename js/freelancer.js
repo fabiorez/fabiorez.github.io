2db3755fc6a62ef14d6e974ff1595d12c53d57ea
fabio rezende <fabio.rezende@gmail.com>
2021-07-03 09:51:24 -0300

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -8,7 +8,8 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 71)
+          scrollTop: (target.offset().top - 71);
+Alert("teste");
         }, 1000, "easeInOutExpo");
         return false;
       }
